7c93fd80a6806f59f5e5ef0d2981dc957078675d
Andreys-Gp <109758319+Andreys-Gp@users.noreply.github.com>
2023-08-21 21:28:14 -0500

    reload

--- services/client-service.js
+++ services/client-service.js
@@ -1,48 +1,5 @@
-const crearNuevaLinea=(nombre,email)=>{
-  const linea=document.createElement(`tr`);
-   const contenido=
-            `
-            <td class="td" data-td>${nombre}</td>
-            <td>${email}</td>
-            <td>
-              <ul class="table__button-control">
-                <li>
-                  <a
-                    href="../screens/editar_cliente.html"
-                    class="simple-button simple-button--edit"
-                    >Editar</a
-                  >
-                </li>
-                <li>
-                  <button
-                    class="simple-button simple-button--delete"
-                    type="button"
-                  >
-                    Eliminar
-                  </button>
-                </li>
-              </ul>
-            </td>
-           `;
-           linea.innerHTML=contenido;
-           return linea;
-};
+const listaClientes = () => fetch("http://localhost:3000/perfil").then(respuesta => respuesta.json());
 
-const table=document.querySelector("[data-table]");
-
-const http=new XMLHttpRequest();
-http.open('GET','http://localhost:3000/perfil');
-http.send();
-http.onload= ()=>{
-    const data= JSON.parse(http.response);
-    console.log(data);
-    data.forEach((perfil) => {
-      const nuevaLinea=crearNuevaLinea(perfil.nombre,perfil.email);
-      table.appendChild(nuevaLinea);
-    }); 
-
-    const http2=new XMLHttpRequest();
-    http2.open("GET","http://localhost:3000/perfil/hoy");
-};
-
-
+export const clientService = {
+  listaClientes
+}
